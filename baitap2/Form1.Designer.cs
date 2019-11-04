1ba66281486a6381f1446f32892f281679a9b372
hoanghoa12345 <44923253+hoanghoa12345@users.noreply.github.com>
2019-11-04 08:51:37 +0700

    Initial commit

---
+++ baitap2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace baitap2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTong = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoPt = new System.Windows.Forms.TextBox();
+            this.lbTong = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTong
+            // 
+            this.btnTong.Location = new System.Drawing.Point(49, 360);
+            this.btnTong.Name = "btnTong";
+            this.btnTong.Size = new System.Drawing.Size(90, 33);
+            this.btnTong.TabIndex = 0;
+            this.btnTong.Text = "Tính Tổng";
+            this.btnTong.UseVisualStyleBackColor = true;
+            this.btnTong.Click += new System.EventHandler(this.btnTong_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập số n";
+            // 
+            // txtSoPt
+            // 
+            this.txtSoPt.Location = new System.Drawing.Point(129, 17);
+            this.txtSoPt.Name = "txtSoPt";
+            this.txtSoPt.Size = new System.Drawing.Size(47, 22);
+            this.txtSoPt.TabIndex = 2;
+            // 
+            // lbTong
+            // 
+            this.lbTong.AutoSize = true;
+            this.lbTong.Location = new System.Drawing.Point(194, 368);
+            this.lbTong.Name = "lbTong";
+            this.lbTong.Size = new System.Drawing.Size(64, 17);
+            this.lbTong.TabIndex = 3;
+            this.lbTong.Text = "Tổng là :";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(52, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(699, 261);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(197, 13);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(90, 31);
+            this.btnNhap.TabIndex = 5;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbTong);
+            this.Controls.Add(this.txtSoPt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTong);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoPt;
+        private System.Windows.Forms.Label lbTong;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNhap;
+    }
+}
+
---
+++ baitap2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap2
+{
+    public partial class Form1 : Form
+    {
+        int[] array = new int[20];
+        private TextBox[] txtArray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            int soPhanTu = int.Parse(txtSoPt.Text);
+            lbTong.Text += SumArray(array, soPhanTu).ToString();
+        }
+
+        private void CreateTextBox (int soPhanTu)
+        {
+            #region CreateTxt
+            /*int a = 1;
+
+            for (int i = 0; i < soPhanTu; i++)
+            {
+                TextBox txt = new TextBox() {
+                    Width = 30,
+                    Top =  28,
+                    Left = a * 34
+                };
+                a = a + 1;
+                panel1.Controls.Add(txt);
+            }
+            TextBox[] textBox = new TextBox[20];
+            for (int i = 0; i < soPhanTu; i++)
+            {
+                panel1.Controls.Add(textBox[i]);
+                textBox[i].Text = i.ToString();
+            }*/
+            #endregion
+            int a = 1;
+            txtArray = new TextBox[soPhanTu];
+            for(int i=0;i< soPhanTu;i++)
+            {
+                txtArray[i] = new TextBox()
+                {
+                    Width = 30,
+                    Top = 28,
+                    Left = a * 34
+                };
+                a++;
+                panel1.Controls.Add(txtArray[i]);
+            }
+        }
+
+        private int SumArray(int[] array, int soPhanTu)
+        {
+            int tong = 0;
+
+            for(int i=0;i<soPhanTu;i++)
+            {
+                array[i] = int.Parse(txtArray[i].Text);
+                tong = tong + array[i];
+            }
+            return tong;
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            int soPhanTu = int.Parse(txtSoPt.Text);
+            CreateTextBox(soPhanTu);
+        }
+    }
+}
